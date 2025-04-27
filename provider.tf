a8b9770ebc3d6b4dfb71a0f39d0fa5dda6a2b7da
ars-max <arshad.kdlr@gmail.com>
2025-04-27 16:41:47 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,15 +1,15 @@
- Run tokendito first for account `tokendito --profile alfheim_admin`
+# Run tokendito first for account `tokendito --profile alfheim_admin`
 provider "aws" {
-  region  = var.region
-  alias = "profile" # TODO use for local work
-  profile = "nct-admin"
+  #region  = var.region
+  #alias = "profile" # TODO use for local work
+  #profile = "nct-admin"
 }
 
 provider "aws" {
   region = var.region
-  assume_role {
-    role_arn     = "arn:aws:iam::${var.aws_configurations.account_id}:role/${var.aws_configurations.assumed_role}"
-    session_name = "terraform_access"
+  #assume_role {
+    #role_arn     = "arn:aws:iam::${var.aws_configurations.account_id}:role/${var.aws_configurations.assumed_role}"
+    #session_name = "terraform_access"
   }
   default_tags {
     tags = var.provider_tags
