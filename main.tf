e7d16f3100f226346ba096e3db4f9c49dac093d9
ars-max <arshad.kdlr@gmail.com>
2025-04-27 16:06:42 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,27 @@
+#EC2 instance module
+module "ec2-instances" {
+  source   = "./modules/ec2-instances"
+  for_each = { for instance in var.ec2_instance_configuration.ec2_instance_details : instance.name => instance }
+
+  name = each.key
+
+  ami                                  = each.value.ami
+  instance_type                        = each.value.instance_type
+  volume_size                          = each.value.volume_size
+  volume_type                          = each.value.volume_type
+  availability_zone                    = each.value.availability_zone
+  subnet_ids                           = each.value.subnet_ids
+  vpc_security_group_ids               = each.value.vpc_security_group_ids
+  iam_instance_profile                 = each.value.iam_instance_profile
+  delete_on_termination                = each.value.delete_on_termination
+  disable_api_termination              = each.value.disable_api_termination
+  instance_initiated_shutdown_behavior = each.value.instance_initiated_shutdown_behavior
+  monitoring                           = each.value.monitoring
+  associate_public_ip_address          = each.value.associate_public_ip_address
+  encrypted                            = each.value.encrypted
+  key_name                             = each.value.key_name
+
+  tags = merge(var.provider_tags, each.value.tags, {
+    Name = each.key
+  })
+}
