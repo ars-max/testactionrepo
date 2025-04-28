800f94f01fd8a28f883b46aa955583020ef358b6
ars-max <arshad.kdlr@gmail.com>
2025-04-28 09:52:48 +0530

    Update remotestate.tf

--- remotestate.tf
+++ remotestate.tf
@@ -1,10 +1,9 @@
 terraform {
   backend "s3" {
-    bucket  = "arstest-tfbackend01"
-    key     = "terraform/state" # Path within the bucket to store the state file
-    region  = "us-east-1"      # Replace with your AWS region
-    # The following are optional, but often recommended:
-    # encrypt = true             # Enable server-side encryption
-    # dynamodb_table = "terraform-state-lock" # For state locking (see below)
+    bucket         = "my-terraform-state-bucket033"  # Replace with your S3 bucket name
+    key            = "terraform/state/terraform.tfstate"  # Path to the state file within the bucket
+    region         = "us-east-1"  # Replace with your AWS region
+    dynamodb_table = "terraform-locks"  # Replace with your DynamoDB table name
+    encrypt        = true  # Encrypt the state file at rest
   }
 }
