f58f773ff5232bd2fbc4a0e52926c13f39335f36
ars-max <arshad.kdlr@gmail.com>
2025-04-28 12:04:43 +0530

    Update remotestate.tf

--- remotestate.tf
+++ remotestate.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "my-terraform-state-bucket033"  # Replace with your S3 bucket name
+    bucket         = "my-terraform-state-bucket0331"  # Replace with your S3 bucket name
     key            = "terraform/state/terraform.tfstate"  # Path to the state file within the bucket
     region         = "us-east-1"  # Replace with your AWS region
     dynamodb_table = "terraform-locks"  # Replace with your DynamoDB table name
