051232381f1d9daaae27df20c4c4c418912b1222
ars-max <arshad.kdlr@gmail.com>
2025-04-28 09:51:48 +0530

    Create s3_db.tf

---
+++ backend_setup/s3_db.tf
@@ -0,0 +1,44 @@
+provider "aws" {
+  region = "us-east-1" # Change to your preferred region
+}
+
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "my-terraform-state-bucket" # Change to a globally unique name
+
+  versioning {
+    enabled = true
+  }
+
+  lifecycle {
+    prevent_destroy = true
+  }
+
+  tags = {
+    Name        = "Terraform State Bucket"
+    Environment = "Dev"
+  }
+}
+
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = "terraform-locks"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+  tags = {
+    Name        = "Terraform Lock Table"
+    Environment = "Dev"
+  }
+}
+
+output "s3_bucket_name" {
+  value = aws_s3_bucket.terraform_state.id
+}
+
+output "dynamodb_table_name" {
+  value = aws_dynamodb_table.terraform_locks.id
+}
