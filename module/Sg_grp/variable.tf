738af991fdaa4b864c792450ff4957db397a4be1
ars-max <arshad.kdlr@gmail.com>
2025-04-28 12:11:50 +0530

    Create variable.tf

---
+++ module/Sg_grp/variable.tf
@@ -0,0 +1,11 @@
+variable "sg_name" {
+  description = "The name of the security group"
+  type        = string
+}
+
+variable "sg_description" {
+  description = "The description of the security group"
+  type        = string
+}
+
+# Define other variables similarly...
