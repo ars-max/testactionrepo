34a463dcbe9735d746f0456a2a80c191c3a824c9
ars-max <arshad.kdlr@gmail.com>
2025-04-29 12:42:35 +0530

    Update variable.tf

--- module/Sg_grp/variable.tf
+++ module/Sg_grp/variable.tf
@@ -1,11 +1,32 @@
-variable "sg_name" {
-  description = "The name of the security group"
-  type        = string
+variable "vpc_id" {
+  type = string
+  description = "The ID of the VPC"
 }
 
-variable "sg_description" {
-  description = "The description of the security group"
-  type        = string
+variable "ingress_from_port" {
+  type = number
+  description = "The starting port for the ingress rule"
 }
 
-# Define other variables similarly...
+variable "ingress_to_port" {
+  type = number
+  description = "The ending port for the ingress rule"
+}
+
+variable "ingress_protocol" {
+  type = string
+  description = "The protocol for the ingress rule (e.g., tcp, udp, icmp)"
+}
+
+variable "ingress_cidr_blocks" {
+  type = list(string)
+  description = "A list of CIDR blocks to allow ingress from"
+}
+
+# ... similarly define variables for egress rules ...
+
+variable "tags" {
+  type = map(string)
+  description = "A map of tags to apply to the security group"
+  default = {}
+}
