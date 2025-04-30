b25e17dfb281d39b9dcf9a4e84ccdcc5dbb97293
ars-max <arshad.kdlr@gmail.com>
2025-04-30 23:34:42 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,25 +1,24 @@
 # EC2 instance module
 module "ec2-instances" {
-  source   = "./module/ec2-instances"
+  source = "./module/ec2-instances"
   for_each = { for instance in var.ec2_instance_configuration.ec2_instance_details : instance.name => instance }
 
-  name = each.key
-
-  ami                                  = each.value.ami
-  instance_type                        = each.value.instance_type
-  volume_size                          = each.value.volume_size
-  volume_type                          = each.value.volume_type
-  availability_zone                    = each.value.availability_zone
-  subnet_ids                           = each.value.subnet_ids
-  vpc_security_group_ids               = each.value.vpc_security_group_ids
-  delete_on_termination                = each.value.delete_on_termination
-  disable_api_termination              = each.value.disable_api_termination
+  name                                     = each.key
+  ami                                      = each.value.ami
+  instance_type                            = each.value.instance_type
+  volume_size                              = each.value.volume_size
+  volume_type                              = each.value.volume_type
+  availability_zone                        = each.value.availability_zone
+  subnet_ids                               = each.value.subnet_ids
+  vpc_security_group_ids                   = each.value.vpc_security_group_ids
+  delete_on_termination                    = each.value.delete_on_termination
+  disable_api_termination                = each.value.disable_api_termination
   instance_initiated_shutdown_behavior = each.value.instance_initiated_shutdown_behavior
-  monitoring                           = each.value.monitoring
-  associate_public_ip_address          = each.value.associate_public_ip_address
-  encrypted                            = each.value.encrypted
-  key_name                             = each.value.key_name
-  tags                                 = each.value.tags
+  monitoring                               = each.value.monitoring
+  associate_public_ip_address              = each.value.associate_public_ip_address
+  encrypted                                = each.value.encrypted
+  key_name                                 = each.value.key_name
+  tags                                     = each.value.tags
 }
 
 resource "aws_vpc" "main" {
@@ -32,7 +31,7 @@ resource "aws_vpc" "main" {
 module "Sg_grp" {
   source = "./module/Sg_grp"
 
-  for_each = {for sg in jsondecode(file("config/var.json")) : sg.Name => sg} # Define for_each only once
+  for_each = { for sg in var.security_group_configurations : sg.Name => sg }
 
   vpc_id              = each.value.vpc_id
   ingress_from_port   = each.value.ingress_from_port
