45583026b11bd0da323b1a0b93f9f8365e77463c
ars-max <arshad.kdlr@gmail.com>
2025-05-02 10:54:52 +0530

    Update main.tf

--- module/Sg_grp/main.tf
+++ module/Sg_grp/main.tf
@@ -1,38 +1,31 @@
-#create a new security group
-resource "aws_security_group" "securitygroup" {
-  name        = var.name
-  description = var.description
-  vpc_id      = var.vpc_id
-  tags        = var.tags
-}
+resource "aws_security_group" "sg" {
+  for_each = {for item in var.security_group_config : item.name => item}
 
-resource "aws_security_group_rule" "ingress_rules" {
-  for_each = merge(
-    { for idx, rule in var.security_group_config.ingress_rules_cidr_blocks : "cidr_${idx}" => rule },
-    { for idx, rule in var.security_group_config.ingress_rules_security_group : "sg_${idx}" => rule }
-  )
+  name        = each.value.name
+  description = each.value.description
+  vpc_id      = each.value.vpc_id
 
-  type              = "ingress"
-  from_port         = each.value.from_port
-  to_port           = each.value.to_port
-  protocol          = each.value.protocol
-  description       = each.value.description
-  security_group_id = aws_security_group.securitygroup.id
+  dynamic "ingress" {
+    for_each = each.value.ingress_rules_cidr_blocks
+    content {
+      from_port   = ingress.value.from_port
+      to_port     = ingress.value.to_port
+      protocol    = ingress.value.protocol
+      cidr_blocks = ingress.value.cidr_blocks
+      description = ingress.value.description
+    }
+  }
 
-  # Conditional assignment: If `cidr_blocks` exist, use it; otherwise, use `source_security_group_id`
-  cidr_blocks              = lookup(each.value, "cidr_blocks", null)
-  source_security_group_id = lookup(each.value, "source_security_group_id", null)
-}
+  dynamic "egress" {
+    for_each = each.value.egress_rules
+    content {
+      from_port   = egress.value.from_port
+      to_port     = egress.value.to_port
+      protocol    = egress.value.protocol
+      cidr_blocks = egress.value.cidr_blocks
+      description = egress.value.description
+    }
+  }
 
-
-resource "aws_security_group_rule" "egress" {
-  for_each = { for idx, rule in var.security_group_config.egress_rules : idx => rule }
-
-  type              = "egress"
-  from_port         = each.value.from_port
-  to_port           = each.value.to_port
-  protocol          = each.value.protocol
-  cidr_blocks       = each.value.cidr_blocks
-  security_group_id = aws_security_group.securitygroup.id
-  depends_on        = [aws_security_group.securitygroup]
+  tags = each.value.tags
 }
