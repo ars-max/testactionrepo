ed9d715d2f3591beaf0650864b6dfa1a5f680ada
ars-max <arshad.kdlr@gmail.com>
2025-06-25 18:58:30 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -84,25 +84,9 @@ variable "tg_configuration" {
       target_group_protocol = string
       vpc_id                = string
       target_type           = string
-      stickiness_enabled    = bool
       tags = object({
         Owner = string
       })
-      # THESE are the attributes made optional within the object type
-      health_check_enabled  = optional(bool)
-      health_check_interval = optional(number)
-      health_check_path     = optional(string)
-      health_check_protocol = optional(string)
-      health_check_timeout  = optional(number)
-      healthy_threshold     = optional(number)
-      unhealthy_threshold   = optional(number)
-      slow_start_duration   = optional(number) # Corrected typo if needed
-      stickiness_type       = optional(string)
-      cookie_duration       = optional(number)
-      cookie_name           = optional(string)
-      successCode_matcher   = optional(string)
-      lambda_multi_value_headers_enabled = optional(bool)
-      proxy_protocol_v2     = optional(bool)
-    }))
-  })
-}
+    })) # Closing parenthesis for list(object({ ... }))
+  })   # Closing curly brace for object({ ... })
+}      # Closing curly brace for variable "tg_configuration"
