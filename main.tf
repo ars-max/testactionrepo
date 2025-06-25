53712ad9e0a6166e6b9c331c35799561b530f6ae
ars-max <arshad.kdlr@gmail.com>
2025-06-25 17:56:21 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -45,3 +45,32 @@ module "loadbalncer" {
   enable_deletion_protection = each.value.enable_deletion_protection
   tags                       = each.value.tags
 }
+#Target group resource calling
+module "target_group" {
+  source   = "./modules/target-group"
+  for_each = { for tg in var.tg_configuration.tg_details : tg.name => tg }
+  vpc_id   = each.value.vpc_id
+  name     = each.key
+
+  target_type                        = each.value.target_type
+  target_group_port                  = each.value.target_group_port
+  health_check_protocol              = each.value.health_check_protocol
+  health_check_interval              = each.value.health_check_interval
+  health_check_timeout               = each.value.health_check_timeout
+  healthy_threshold                  = each.value.healthy_threshold
+  unhealthy_threshold                = each.value.unhealthy_threshold
+  health_check_path                  = each.value.health_check_path
+  healthCheckEnabled                 = each.value.healthCheckEnabled
+  health_check_port                  = each.value.health_check_port
+  target_group_protocol              = each.value.target_group_protocol
+  cookie_duration                    = each.value.cookie_duration
+  deregistration_delay               = each.value.deregistration_delay
+  slow_start_duruation               = each.value.slow_start_duruation
+  stickiness_enabled                 = each.value.stickiness_enabled
+  stickiness_type                    = each.value.stickiness_type
+  successCode_matcher                = each.value.successCode_matcher
+  cookie_name                        = each.value.cookie_name
+  lambda_multi_value_headers_enabled = each.value.lambda_multi_value_headers_enabled
+  proxy_protocol_v2                  = each.value.proxy_protocol_v2
+  tags                               = each.value.tags
+}
